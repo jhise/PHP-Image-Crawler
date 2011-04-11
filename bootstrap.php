4fb00354c815da8804ec90b8d52b9e7ca6c626cd
Jeremy <jeremy.hise@gmail.com>
2011-04-11 17:40:51 -0400

    Setting up basic framework

---
+++ bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+	$base = dirname(realpath(__FILE__));
+
+	$paths	= implode(PATH_SEPARATOR, 
+							array(	get_include_path(),
+									$base.PATH_SEPARATOR.'library',
+									$base.PATH_SEPARATOR.'config'));
+
+	set_include_path($paths);
+
+	spl_autoload_register('auto_load');
+
+	function auto_load($class)
+	{
+		@include_once($class.'.php');	
+	}
